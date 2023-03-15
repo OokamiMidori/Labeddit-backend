92b219875669ce00005f602e6fea4232a0745490
OokamiMidori <fabio.miranda.couto@gmail.com>
2023-03-14 21:10:40 -0300

    tentando debugar

---
+++ src/database/CommentDataBase.ts
@@ -0,0 +1,6 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class CommentDataBase extends BaseDatabase {
+    public static TABLE_COMMENTS = "comments"
+
+}
--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,62 @@ export enum USER_ROLES {
 
 export interface TokenPayload {
     id: string,
-	name: string,
+    name: string,
     role: USER_ROLES
+}
+
+export interface UserDB {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: USER_ROLES,
+    created_at: string
+}
+
+export interface PostDB {
+    id: string,
+    creator_id: string,
+    content: string,
+    likes: number,
+    dislikes: number,
+    created_at: string,
+    updated_at: string
+}
+
+export interface PostWithCreatorDB extends PostDB {
+    creator_name: string
+}
+
+export interface LikeDislikeDB {
+    user_id: string,
+    post_id: string,
+    like: number
+}
+
+export enum POST_LIKE {
+    ALREADY_LIKED = "ALREADY_LIKED",
+    ALREADY_DISLIKED = "ALREADY_DISLIKED"
+}
+
+export interface PostModel {
+    id: string,
+    content: string,
+    likes: number,
+    dislikes: number,
+    createdAt: string,
+    updatedAt: string,
+    creator: {
+        id: string,
+        name: string
+    }
+}
+
+export interface UserModel {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: string,
+    createdAt: string
 }
